5368915bf56f41e894f74a597a845898d33a492c
prashansatanwar <prashansatanwar@gmail.com>
2024-05-03 22:18:50 +0800

    Projects page

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Projects from './pages/Projects';
+
+function App() {
+  return (
+    <div className="App bg-[#ccd5ae]">
+     <Projects/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data/projects.js
@@ -0,0 +1,50 @@
+export const projects = [
+    {
+        title: "Mind Meld",
+        date: "March 2024",
+        description: "Conceptualized and executed a full stack application leveraging the OpenAI API and MongoDB to analyze resumes and craft customized interview questions. The project aims to enhance interview readiness by providing tailored preparation questions and insightful feedback, showcasing a blend of technical prowess and proactive problem-solving.",
+        tags: ["All","Python", "Flask", "MongoDB", "ReactJS"],
+        github: "https://github.com/prashansatanwar/MindMeld",
+        link: "https://mind-meld-beta.vercel.app/",
+        image: "assets/images/mindmeld.png",
+    },
+    {
+        title: "RShell",
+        date: "March 2024",
+        description: "This project is a custom Linux shell created using Rust, featuring support for fundamental Linux commands.",
+        tags: ["All","Rust"],
+        github: "https://github.com/prashansatanwar/shell-rust",
+        link: "",
+        image: "assets/images/shellrust.png",
+    },
+    {
+        title: "Ping Pong",
+        date: "April 2024",
+        description: "A desktop application of a 2 player ping-pong game implemented in Rust",
+        tags: ["All","Rust", "Game"],
+        github: "https://github.com/prashansatanwar/ping-pong-rust",
+        link: "",
+        image: "assets/images/pingpong.png",
+    },
+    {
+        title: "Tetris",
+        date: "May 2024",
+        description: "A desktop application of Tetris implemented in Rust",
+        tags: ["All","Rust", "Game"],
+        github: "https://github.com/prashansatanwar/tetris-rust",
+        link: "",
+        image: "assets/images/tetris.png",
+    },
+
+];
+
+
+// {
+//     title: "",
+//     date: "",
+//     description: "",
+//     tags: [],
+//     github: "",
+//     link: "",
+//     image:"",
+// }
